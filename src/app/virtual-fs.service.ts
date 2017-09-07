209a2540248a79099b892d9b122175dc539b44bd
Abhimanyu Deora <akdeora@google.com>
2017-09-07 11:20:53 -0700

    update default index.html to properly load global styles.css in example app

--- src/app/virtual-fs.service.ts
+++ src/app/virtual-fs.service.ts
@@ -222,7 +222,7 @@ export class VirtualFsService {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Document</title>
-  <link rel="stylesheet" href="/assets/dist/styles.css" />
+  <link rel="stylesheet" href="/dist/styles.css" />
   <!-- uncomment one of these to use @angular/material pre-built themes -->
   <!-- <link rel="stylesheet" href="https://unpkg.com/@angular/material@2.0.0-beta.10/prebuilt-themes/deeppurple-amber.css" /> -->
   <!-- <link rel="stylesheet" href="https://unpkg.com/@angular/material@2.0.0-beta.10/prebuilt-themes/indigo-pink.css" /> -->
